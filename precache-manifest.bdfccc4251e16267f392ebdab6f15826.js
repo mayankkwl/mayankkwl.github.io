276133b474ed704da76c0ffd4ee28669f63eaaab
Mayank Khandelwal <mayank.khandelwal@gmail.com>
2019-06-17 18:24:43 +0100

    Builld version updated

---
+++ precache-manifest.bdfccc4251e16267f392ebdab6f15826.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5050feb19d49fc610d566809b0f342f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c80a04ad58461270d602",
+    "url": "/static/css/main.59d3655b.chunk.css"
+  },
+  {
+    "revision": "62b3b0d5d79c90a47775",
+    "url": "/static/js/2.ce6e9024.chunk.js"
+  },
+  {
+    "revision": "c80a04ad58461270d602",
+    "url": "/static/js/main.2e3617db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dae8cc0eddd99ef3e2278a78c527e027",
+    "url": "/static/media/Carnas-Bold.dae8cc0e.otf"
+  },
+  {
+    "revision": "89a4601e1a909640fb76cb9c52252161",
+    "url": "/static/media/Carnas-Lig.89a4601e.otf"
+  },
+  {
+    "revision": "00c9bb0f518b34a19ad49bb764477545",
+    "url": "/static/media/Carnas-Med.00c9bb0f.otf"
+  },
+  {
+    "revision": "066b65cc52c4d75ccdac9bd43687e4d9",
+    "url": "/static/media/Carnas-Reg.066b65cc.otf"
+  },
+  {
+    "revision": "a9e6ce67ddae2da0045538c14b36013c",
+    "url": "/static/media/Carnas-Thin.a9e6ce67.otf"
+  },
+  {
+    "revision": "1b8d76d369412c6dc7a05f8de8170dd7",
+    "url": "/static/media/login-image.1b8d76d3.png"
+  },
+  {
+    "revision": "71ebf319474db2ecc9cad079ef9f34bd",
+    "url": "/static/media/spinner.71ebf319.gif"
+  }
+]);
