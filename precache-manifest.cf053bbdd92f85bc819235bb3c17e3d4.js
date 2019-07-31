baa3369f47615b8f9086b24bed2ee07889f7fe6f
Mayank Khandelwal <mayank.khandelwal@gmail.com>
2019-07-31 11:59:16 +0100

    Build updated

---
+++ precache-manifest.cf053bbdd92f85bc819235bb3c17e3d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3fb8481af84013f74c3bf35b7dced7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04901bbcc28b14a552e8",
+    "url": "/static/css/main.a818998e.chunk.css"
+  },
+  {
+    "revision": "afe3bf6a4542137b66aa",
+    "url": "/static/js/2.0a0fd616.chunk.js"
+  },
+  {
+    "revision": "04901bbcc28b14a552e8",
+    "url": "/static/js/main.715da92d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
